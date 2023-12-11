58e14f23c38888ad8081025c3d4b2b6f97d8caef
Yanis <yanis.oultaf@edu.esiee-it.fr>
2023-12-11 18:14:49 +0100

    Add animation to the 1stPerson plane

--- Assets/Scripts/Test/PlaneAnimation.cs
+++ Assets/Scripts/Test/PlaneAnimation.cs
@@ -11,12 +11,17 @@ public class PlaneAnimation : MonoBehaviour {
     [Header ("Aileron (Roll)")]
     public Transform aileronLeft;
     public Transform aileronRight;
+    public Transform aileronLeft1StP;
+    public Transform aileronRight1StP;
     public float aileronMax = 20;
     [Header ("Elevator (Pitch)")]
     public Transform elevator;
+    public Transform elevatorLeft1StP;
+    public Transform elevatorRight1StP;
     public float elevatorMax = 20;
     [Header ("Rudder (Yaw)")]
     public Transform rudder;
+    public Transform rudder1stP;
     public float rudderMax = 20;
 
     // Smoothing vars
@@ -35,22 +40,27 @@ public class PlaneAnimation : MonoBehaviour {
 
     void Update () {
         // https://en.wikipedia.org/wiki/Aircraft_principal_axes
-        propeller.Rotate (Vector3.forward * propSpeed * Time.deltaTime);
+        propeller.Rotate (Vector3.forward * (propSpeed * Time.deltaTime));
 
         // Roll
         float targetRoll = plane.Roll;
         smoothedRoll = Mathf.SmoothDamp (smoothedRoll, targetRoll, ref smoothRollV, Time.deltaTime * smoothTime);
         aileronLeft.localEulerAngles = new Vector3 (-smoothedRoll * aileronMax, aileronLeft.localEulerAngles.y, aileronLeft.localEulerAngles.z);
+        aileronLeft1StP.localEulerAngles = new Vector3 (-smoothedRoll * aileronMax, aileronLeft1StP.localEulerAngles.y, aileronLeft1StP.localEulerAngles.z);
         aileronRight.localEulerAngles = new Vector3 (smoothedRoll * aileronMax, aileronRight.localEulerAngles.y, aileronRight.localEulerAngles.z);
+        aileronRight1StP.localEulerAngles = new Vector3 (smoothedRoll * aileronMax, aileronRight1StP.localEulerAngles.y, aileronRight1StP.localEulerAngles.z);
 
         // Pitch
         float targetPitch = plane.Pitch;
         smoothedPitch = Mathf.SmoothDamp (smoothedPitch, targetPitch, ref smoothPitchV, Time.deltaTime * smoothTime);
         elevator.localEulerAngles = new Vector3 (-smoothedPitch * elevatorMax, elevator.localEulerAngles.y, elevator.localEulerAngles.z);
+        elevatorLeft1StP.localEulerAngles = new Vector3 (-smoothedPitch * elevatorMax, elevator.localEulerAngles.y, elevator.localEulerAngles.z);
+        elevatorRight1StP.localEulerAngles = new Vector3 (-smoothedPitch * elevatorMax, elevator.localEulerAngles.y, elevator.localEulerAngles.z);
 
         // Yaw
         float targetYaw = plane.Yaw;
         smoothedYaw = Mathf.SmoothDamp (smoothedYaw, targetYaw, ref smoothYawV, Time.deltaTime * smoothTime);
         rudder.localEulerAngles = new Vector3 (rudder.localEulerAngles.x, -smoothedYaw * rudderMax, rudder.localEulerAngles.z);
+        rudder1stP.localEulerAngles = new Vector3 (rudder.localEulerAngles.x, -smoothedYaw * rudderMax, rudder.localEulerAngles.z);
     }
 }
